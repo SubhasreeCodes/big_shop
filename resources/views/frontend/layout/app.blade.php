3ff33083061ec45beccd290d725c0632a1e286b4
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-16 12:30:08 +0530

    Update app.blade.php

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -25,6 +25,9 @@
     <!-- Include the Footer partial -->
     @include('frontend.layout.footer')
 
+    <!-- Include the Preloader partial -->
+    @include('frontend.preloader.type1')
+
 <!-- Vendor JS-->
 <script src="frontend/js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="frontend/js/vendor/jquery-3.6.0.min.js"></script>
