bd3c5e6a3e210a1973f1441f432e84bdbf077d15
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-16 18:16:41 +0530

    Update app.blade.php
    slider code added

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -23,6 +23,13 @@
     <!-- Include the Topbar partial -->
     @include('frontend.layout.topbar')
 
+    <main class="main">
+
+    <!-- Include the Slider partial -->
+    @include('frontend.slider.type1')
+
+</main>    
+
     @yield('content')
 
     <!-- Include the Footer partial -->
