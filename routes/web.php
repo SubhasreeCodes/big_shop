f11c86df4ec53133772c36c687e35ef48e99532c
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-17 19:09:58 +0530

    Update web.php
    Updated web.php with login's route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 Route::get('/forget_password',[AuthController::class,'forget_password'])->name('home.forget_password');
 Route::get('/home',[HomeController::class,'index'])->name('home.index');
+Route::get('/login',[HomeController::class,'login'])->name('home.login');
