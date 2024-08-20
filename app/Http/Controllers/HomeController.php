a41dceda6b52a923fe88445397e1624b91e81ac4
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-20 18:21:18 +0530

    Upadate
    Updated the code(From HomeController to AuthController)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,53 +6,5 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request)
-    {
-        return view('frontend/home');
-    }
-
-    public function login(Request $request)
-    {
-        return view('frontend/auth/login');
-    }
-
-    public function register(Request $request)
-    {
-        return view('frontend/auth/register');
-    }
-
-    public function forget_password(Request $request)
-    {
-        return view('frontend/auth/forget_password');
-    }
-
-    public function reset_password(Request $request)
-    {
-        return view('frontend/auth/reset_password');
-    }
-
-    public function my_account(Request $request)
-    {
-        return view('frontend/auth/my_account');
-    }
-
-    public function page_terms(Request $request)
-    {
-        return view('frontend/auth/page_terms');
-    }
-
-    public function privacy_policy(Request $request)
-    {
-        return view('frontend/auth/privacy_policy');
-    }
-
-    public function purchase_guide(Request $request)
-    {
-        return view('frontend/auth/purchase_guide');
-    }
-
-    public function about(Request $request)
-    {
-        return view('frontend/auth/about');
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
 Route::get('/forget_password',[AuthController::class,'forget_password'])->name('home.forget_password');
-Route::get('/home',[HomeController::class,'index'])->name('home.index');
-Route::get('/login',[HomeController::class,'login'])->name('home.login');
-Route::get('/register',[HomeController::class,'register'])->name('home.register');
-Route::get('/forget_password',[HomeController::class,'forget_password'])->name('home.forget_password');
-Route::get('/reset_password',[HomeController::class,'reset_password'])->name('home.reset_password');
-Route::get('/my_account',[HomeController::class,'my_account'])->name('home.my_account');
-Route::get('/page_terms',[HomeController::class,'page_terms'])->name('home.page_terms');
-Route::get('/privacy_policy',[HomeController::class,'privacy_policy'])->name('home.privacy_policy');
-Route::get('/purchase_guide',[HomeController::class,'purchase_guide'])->name('home.purchase_guide');
-Route::get('/about',[HomeController::class,'about'])->name('home.about');
+Route::get('/home',[AuthController::class,'index'])->name('home.index');
+Route::get('/login',[AuthController::class,'login'])->name('home.login');
+Route::get('/register',[AuthController::class,'register'])->name('home.register');
+Route::get('/reset_password',[AuthController::class,'reset_password'])->name('home.reset_password');
+Route::get('/my_account',[AuthController::class,'my_account'])->name('home.my_account');
+Route::get('/page_terms',[AuthController::class,'page_terms'])->name('home.page_terms');
+Route::get('/privacy_policy',[AuthController::class,'privacy_policy'])->name('home.privacy_policy');
+Route::get('/purchase_guide',[AuthController::class,'purchase_guide'])->name('home.purchase_guide');
+Route::get('/about',[AuthController::class,'about'])->name('home.about');
 
