06089990bd15119889f0b86b55e9b1afc20b3e73
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-22 18:54:16 +0530

    Updated purchase_guide

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,30 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\AuthController;
+
 Route::get('/forget_password',[AuthController::class,'forget_password'])->name('home.forget_password');
+
 Route::get('/',[AuthController::class,'index'])->name('home.index');
+
 Route::get('/login',[AuthController::class,'login'])->name('home.login');
+
 Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('home.logout');
 
 Route::get('/register',[AuthController::class,'register'])->name('home.register');
+
 Route::get('/reset_password',[AuthController::class,'reset_password'])->name('home.reset_password');
+
 Route::get('/my_account',[AuthController::class,'my_account'])->name('home.my_account');
+
 Route::get('/page_terms',[AuthController::class,'page_terms'])->name('home.page_terms');
+
 Route::get('/privacy_policy',[AuthController::class,'privacy_policy'])->name('home.privacy_policy');
+
 Route::get('/purchase_guide',[AuthController::class,'purchase_guide'])->name('home.purchase_guide');
+
 Route::get('/about',[AuthController::class,'about'])->name('home.about');
+
 Route::get('/error_page',[AuthController::class,'error_page'])->name('home.error_page');
 
